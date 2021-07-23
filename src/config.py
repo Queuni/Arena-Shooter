c5f1a227b467523dc966c6d03c0179a7299f1cb2
Queuni <Queuni@users.noreply.github.com>
2021-07-23 15:55:09 +0000

    Correct the default path used when no config file is specified

--- src/config.py
+++ src/config.py
@@ -8,3 +8,5 @@
 # Correct the default so it matches what the documentation says
 
 # Update documentation to reflect the new API and usage examples
+
+# Clean up the deprecated alias and point callers to the new name
