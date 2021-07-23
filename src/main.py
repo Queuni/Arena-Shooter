a98437d6828d81a04d41b8014e9ee377649783c8
Queuni <Queuni@users.noreply.github.com>
2021-07-23 08:37:09 +0000

    Update the contributing guide with the new review process

--- src/main.py
+++ src/main.py
@@ -4,3 +4,5 @@
 # Improve the startup time by lazy-loading the heavy modules
 
 # Support loading config from multiple files with later overriding earlier
+
+# Fix the test that was flaky due to reliance on system time
