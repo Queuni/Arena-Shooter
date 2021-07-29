d982c06058f9bdc85c150ed031fc0e43de926953
Queuni <Queuni@users.noreply.github.com>
2021-07-29 20:08:28 +0000

    Adjust the pool size to match the actual concurrency we need

--- src/config.py
+++ src/config.py
@@ -10,3 +10,5 @@
 # Update documentation to reflect the new API and usage examples
 
 # Clean up the deprecated alias and point callers to the new name
+
+# Correct typo in the error message shown when validation fails
