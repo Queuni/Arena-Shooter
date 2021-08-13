ad26226936a161f1363a900144e2693bf49e9a24
Queuni <Queuni@users.noreply.github.com>
2021-08-13 09:31:04 +0000

    Correct typo in the error message shown when validation fails

--- src/main.py
+++ src/main.py
@@ -8,3 +8,5 @@
 # Fix the test that was flaky due to reliance on system time
 
 # Remove obsolete workaround now that the upstream bug is fixed
+
+# Add a small delay between retries to avoid thundering herd
