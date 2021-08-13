26fcfec32e5e5ffbfa01663e97c4ae83f49485a0
Queuni <Queuni@users.noreply.github.com>
2021-08-13 09:04:27 +0000

    Handle edge case when the response body is empty but status is 200

--- src/main.py
+++ src/main.py
@@ -6,3 +6,5 @@
 # Support loading config from multiple files with later overriding earlier
 
 # Fix the test that was flaky due to reliance on system time
+
+# Remove obsolete workaround now that the upstream bug is fixed
